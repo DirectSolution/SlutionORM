ac07a0aa199b5f21251667eb2c910525190be5e5
Doug <doug@hsdirect.co.uk>
2016-03-04 16:40:22 +0000

    This

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-use SolutionORM\SolutionORM,
-    PDO;
 if (!interface_exists('JsonSerializable')) {
 	interface JsonSerializable {
 		function jsonSerialize();
@@ -9,14 +7,3 @@ if (!interface_exists('JsonSerializable')) {
 require_once dirname(__FILE__) . "/vendor/autoload.php";
 ini_set('display_errors', 'On');
 error_reporting(E_ALL);
-
-$connection = new PDO("mysql:dbname=prod_portal; host=89.151.79.10", 'hsdirect', '9Etr8F*uBr');
-$a = new SolutionORM($connection);
-
-foreach($a->mast_users->order("username")->limit(100) AS $user){
-    print "<pre>";
-    var_dump($user['username']);
-    print "</pre>";
-}
-
-print "HERE";
